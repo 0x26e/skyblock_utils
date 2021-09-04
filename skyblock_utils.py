5104112e9706bd08b0426f583f827f3f45e48744
0x26e <78525013+0x26e@users.noreply.github.com>
2021-09-03 20:57:44 -0400

    Episode 1.1

--- skyblock_utils.py
+++ skyblock_utils.py
@@ -1,39 +1,16 @@
+# Imports
 import requests
 import json
 from pprint import pprint
 
+# Functions
 def get_info(call):
     r = requests.get(call)
     return r.json()
 
+# Variables
 API_FILE = open("API_KEY.json", "r")
 API_KEY = json.loads(API_FILE.read())["API_KEY"]
 
-pprint(API_KEY)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
+# Code
